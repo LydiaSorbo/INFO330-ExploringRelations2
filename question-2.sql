93b7e098b461702a9fbd2c607f64ede8f8cadaa0
LydiaSorbo <43254564+LydiaSorbo@users.noreply.github.com>
2023-04-07 13:00:31 -0700

    Update question-2.sql

--- question-2.sql
+++ question-2.sql
@@ -1,1 +1,2 @@
 -- Show the sales agent's full name and invoices associated with each sales agent.
+select employees.FirstName, employees.LastName, invoices.* from customers, employees, invoices where customers.supportRepID = employees.employeeId and customers.customerId = invoices.customerId;
