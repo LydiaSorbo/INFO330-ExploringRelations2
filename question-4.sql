4e319fd7c8c7e3fd54da525dc3a6542eb46c1d03
LydiaSorbo <43254564+LydiaSorbo@users.noreply.github.com>
2023-04-07 13:24:08 -0700

    Update question-4.sql

--- question-4.sql
+++ question-4.sql
@@ -1,1 +1,2 @@
 -- Which sales agent made the most in sales in 2010?
+select count(invoices.invoiceId) as 'Number of Sales in 2010', employees.FirstName, employees.LastName from invoices, employees, customers where customers.supportRepId = employees.employeeId and invoices.InvoiceDate like '%2010%' group by employees.FirstName;
