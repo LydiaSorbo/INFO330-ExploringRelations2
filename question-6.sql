232e9ca6507c67017e2de8e08f9ff305d3bf9440
LydiaSorbo <43254564+LydiaSorbo@users.noreply.github.com>
2023-04-07 13:53:04 -0700

    Update question-6.sql

--- question-6.sql
+++ question-6.sql
@@ -1,1 +1,2 @@
 -- Find tracks (id, name and composer) that are part of a line in an invoice.
+select tracks.trackId, tracks.Name, tracks.composer from invoices, invoice_items, tracks where invoices.d = invoice_items.invoiceId and invoice_items.trackId = tracks.trackId;
