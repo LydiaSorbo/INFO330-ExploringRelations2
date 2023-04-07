dc43905f4ebf92a1b86f26371de5e85d99115698
LydiaSorbo <43254564+LydiaSorbo@users.noreply.github.com>
2023-04-07 13:45:40 -0700

    Update question-5.sql

--- question-5.sql
+++ question-5.sql
@@ -1,1 +1,2 @@
 -- What was the most purchased track of 2013?
+select count(tracks.Name) as 'Number of Purchases in 2013', tracks.Name from invoices, invoice_items, tracks where invoices.invoiceId = invoice_items.invoiceId and invoice_items.trackId = tracks.trackId and invoices.invoiceDate like '%2013%' group by tracks.name order by count(tracks.Name) desc;
